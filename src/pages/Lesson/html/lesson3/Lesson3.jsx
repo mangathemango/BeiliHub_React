50a7e00119d113aefd1d29bbbb07880d1fa40fc7
Inkt13 <138111203+Inkt13@users.noreply.github.com>
2025-10-12 15:13:02 +0800

    I added lesson 3 and 7

--- src/pages/Lesson/html/lesson3/Lesson3.jsx
+++ src/pages/Lesson/html/lesson3/Lesson3.jsx
@@ -1,11 +1,11 @@
 import "./Lesson3.css"
+import Question from "../../components/Question/Question"
+import quizData from "./quiz.json"
                     
 const Lesson3 = () => {
-    return <div>
-        <p>This is lesson 3 from html</p>
-        <p>This lesson is probably not done yet</p>
+    return <div style={{ height: "400px" }}>
+        <Question quizData={quizData}></Question>
     </div>
 }
-
 export default Lesson3;
             
