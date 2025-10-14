f12e8200065919ea6987058edadf3d6ef7ecd23d
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-10-14 23:32:49 +0800

    Remove some unnecessary components

--- src/pages/Lesson/components/Editor/components/DescriptionBlock/components/QuizWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// QuizWrapper component for DescriptionBlock
-const QuizWrapper = ({ children }) => <div data-quiz>{children}</div>;
-
-QuizWrapper.displayName = 'Quiz';
-
-export default QuizWrapper;
--- src/pages/Lesson/components/Editor/components/DescriptionBlock/components/TaskWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-// TaskWrapper component for DescriptionBlock
-const TaskWrapper = ({ children }) => <div data-task>{children}</div>;
-
-TaskWrapper.displayName = 'TaskWrapper';
-
-export default TaskWrapper;
